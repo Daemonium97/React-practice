75000d6913fc84d7bd8bfa6492ac3881ab6996d8
Daemonium97 <daemonium97@gmail.com>
2021-02-04 18:33:18 +0000

    react practice

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,31 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import { Navbar } from "./navbar.js";
+import { Boilerplate } from "./boilerplate";
+import { MyCard } from "./card";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<Boilerplate
+				title="Qué es Google?"
+				subtitle="Google, LLC es una compañía principal subsidiaria de la multinacional estadounidense Alphabet Inc., cuya especialización son los productos y servicios relacionados con Internet, software, dispositivos electrónicos y otras tecnologías."
+				description="Es algo que nos ayuda a todos en la vida."
+				label="Aprenda sobre gugul"
+				link="https://es.wikipedia.org/wiki/Google"
+			/>
+			<MyCard
+				cardTitle1="Finn"
+				description1="asd"
+				cardTitle2="Jake"
+				description2="sda"
+				cardTitle3="BMO"
+				description3="asd"
+			/>
 		</div>
 	);
 }
